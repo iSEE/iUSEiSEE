527bef511188bce23d8e45989e6d2de8f5f4ce1a
Federico Marini <marinif@uni-mainz.de>
2024-04-29 17:04:46 +0200

    import statements fixed

--- R/iUSEiSEE-pkg.R
+++ R/iUSEiSEE-pkg.R
@@ -1,5 +1,9 @@
 #' iUSEiSEE: Learning to use the package iSEE
 #'
+#' @importFrom iSEE iSEE
+#' @importFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom scater plotTSNE
+#'
 #' @name iUSEiSEE-pkg
 #' @docType package
 #' @keywords internal
